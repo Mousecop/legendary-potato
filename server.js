5d1c9c5809f73944dba57d9df53d66cb38b54eaa
Jean-Luc19 <aticus19@gmail.com>
2017-02-23 13:17:27 -0700

    delete method added and functional

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const {BlogPost} = require('./models');
 app.get('/posts',(req, res) => {
     BlogPost
         .find()
-        .limit(5)
+        .limit(14)
         .exec()
         .then(posts => {
             res.json({
@@ -66,7 +66,37 @@ app.post('/posts/', (req, res) => {
     });
 });
 
+app.put('/posts/:id', (req, res) => {
+  if(!(req.body.id && req.params.id && req.params.id === req.body.id)) {
+    const message = `Request path id ${req.params.id} and request body id ${req.body.id} must match.`;
+    console.error(message);
+    res.status(400).json({message: message});
+  }
+  const toUpdate = {};
+  const updateableFields = ['title', 'content', 'author'];
+  updateableFields.forEach(field => {
+    if(field in req.body) {
+      toUpdate[field] = req.body[field];
+    }
+  });
 
+  BlogPost
+    .findByIdAndUpdate(req.params.id, {$set: toUpdate})
+    .exec()
+    .then(post => {
+      res.status(204).end();
+    })
+    .catch(err => {
+      res.status(500).json({message: 'Internal Server Error ' + err});
+    })
+});
+app.delete('/posts/:id', (req, res) => {
+  BlogPost
+    .findByIdAndRemove(req.params.id)
+    .exec()
+    .then(post => res.status(204).end())
+    .catch(err => res.status(500).json({message: 'Internal Server Error '+ err}));
+});
 
 
 
